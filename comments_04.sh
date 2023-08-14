2b89bfa70211921403f18d8a398bfbdb76140042
1706017 <amritmanash9@gmail.com>
2023-08-14 10:37:49 +0000

    Comments in shell script

---
+++ comments_04.sh
@@ -0,0 +1,12 @@
+#This is an example for single line comment is shell script
+echo "Single line comment"
+
+<<comment
+This is an Example
+of Multi Line 
+comment. 
+comment
+echo "Multi Line comment"
+echo "Bye!!"
+
+
