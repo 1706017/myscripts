2fe027163603d1068caa42a456d573c3f330088c
1706017 <amritmanash9@gmail.com>
2023-08-14 10:49:25 +0000

    constant varibale in shell script

---
+++ constant_variable_06.sh
@@ -0,0 +1,13 @@
+#This script will elaborate the use of constant variable whose value remains unchanged
+
+Name="Amrit"
+
+#This Age variable is constant varible whose value will be unchanged
+readonly Age="25"
+
+echo "Hello $Name your age is $Age"
+
+Name="Manash"
+Age="24"
+
+echo "Hello $Name your age is $Age"
