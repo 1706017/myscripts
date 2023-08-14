cc1405493871168b74324d0e495639cd3f8dacb2
1706017 <amritmanash9@gmail.com>
2023-08-14 10:31:00 +0000

    script for if else

---
+++ if_else_03.sh
@@ -0,0 +1,12 @@
+echo "Please Enter your Age"
+read age
+
+if [[ $age -ge 18 ]]
+then
+        echo "You are eligible for voting!!"
+else
+        echo "You are not eligible for voting!!"
+fi
+
+echo "Bye !!"
+
