429b30c98c5e6275d84c776bf1877158ff52620a
1706017 <amritmanash9@gmail.com>
2023-08-14 11:52:17 +0000

    Arithmetic Operation in shell script

---
+++ Arithmetic_operation_10.sh
@@ -0,0 +1,13 @@
+#Taking user input for two numbers
+echo "Please enter Number 1"
+read num1
+echo "Please enter Number 2"
+read num2
+
+#Now performing all the arithemetic operations 
+echo "Sum of two Numbers is :$(($num1 + $num2))"
+echo "Product of two Numbers is :$(($num1 * $num2))"
+
+
+echo "Bye!!"
+
