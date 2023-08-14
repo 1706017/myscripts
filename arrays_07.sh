4af208b078dcfa97850f07b3b0ebeebf16b7d3a4
1706017 <amritmanash9@gmail.com>
2023-08-14 11:13:10 +0000

    Arrays in shell script

---
+++ arrays_07.sh
@@ -0,0 +1,23 @@
+#Define an array
+
+myArray=( 'Amrit' 25 'NO' 40000 )
+
+#Accesing items from an array
+echo "My name is :${myArray[0]}"
+echo "My Age is : ${myArray[1]}"
+echo "Married : ${myArray[2]}"
+echo "My Salary is : ${myArray[3]}"
+
+
+#How to get the complete length of Array 
+echo "Total items inside array is : ${#myArray[*]}"
+
+#How to print all elements of Array
+echo "All elements are: ${myArray[*]}"
+
+#To update our Array with more items 
+myArray+=( 'Kolkata' 'Devops' )
+echo "Now updated Array is : ${myArray[*]}"
+
+echo "Bye!!"
+
