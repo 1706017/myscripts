28421654af347a00634432a673db8d398ae1fb95
1706017 <amritmanash9@gmail.com>
2023-08-14 10:43:03 +0000

    execute commands in shell

---
+++ execute_Linux_command_05.sh
@@ -0,0 +1,9 @@
+#This script will demonstrate how to execute the command in shell script 
+Date=$(date)
+echo "CURRENT DATE IS :$Date"
+
+Name=$(whoami)
+echo "CURRENT LOGGED IN USER NAME IS :$Name"
+
+
+
