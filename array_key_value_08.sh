e13824d190803283d84676dac93de26212b45788
1706017 <amritmanash9@gmail.com>
2023-08-14 11:20:23 +0000

    Adding key pair value array

---
+++ array_key_value_08.sh
@@ -0,0 +1,12 @@
+#defining the key value pair array
+
+declare -A myArray
+myArray=([name]="Amrit" [age]=25)
+
+#Accesing the value using key of array
+echo "My name is :${myArray[name]}"
+echo "My age is : ${myArray[age]}"
+
+echo "Bye!!"
+
+
