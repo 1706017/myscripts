68af4eb2230cf8f295d9ba92ea2047c1182d0a65
1706017 <amritmanash9@gmail.com>
2023-08-16 15:58:06 +0000

    Arguments in shell script

---
+++ 18_arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "The first argument is $1"
+echo "The second argument is $2"
+echo "ALL the arguments are as follows $@"
+echo "The Number of arguments are $#"
+
+echo "Bye"
+
+
