961abd63b252ae489f322fc0c4c1095de08f55b6
1706017 <amritmanash9@gmail.com>
2023-08-16 15:26:12 +0000

    using arguments with function

---
+++ function_arg_16.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#defining functions
+sayName(){
+ echo "Hi $1 how are you ?"
+}
+
+sayAge(){
+ echo "Your age is $1"
+}
+
+sayBye(){
+ echo "okay $1 take care of yourself "
+}
+
+#calling functions
+sayName Amrit 
+sayAge 25
+sayBye Amrit
+
+
+
