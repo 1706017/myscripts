a15dbd79a19bf42ab1d428da14ffb4334e4d2131
1706017 <amritmanash9@gmail.com>
2023-08-16 15:14:36 +0000

    Function in shell script

---
+++ function_demo_15.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Hi welcome to function demo "
+
+#defining our function
+
+function sayHello {
+	echo "Hello How are you ?"
+}
+
+sayBye(){
+	echo "Bye Bye see you soon !!"
+}
+
+#calling our functions
+sayHello
+sayBye
+
+
